4964e5275b2c70afc641d2e936b0bc1450da3e5f
VIDAL-Antoine <avidal78390@gmail.com>
2023-01-18 17:46:49 +0100

    v2 : améliorations

--- sobel.c
+++ sobel.c
@@ -142,48 +142,33 @@ void sobel_v1(u8 *cframe, u8 *oframe, f32 threshold)
 # Version 2 : Optimisations au niveau du code source
 # Regroupement en une fonction
 # Suppression des matrices f1 et f2 et de la double boucle interne
-# Utilisation de variables temporaires pour alléger les calculs
 # La racine carrée étant un goulot d'étranglement, il ne semble pas possible d'y échapper sans devoir faire des approximations
 ######################################
 */
 
-void sobel_v2(u8 *cframe, u8 *oframe, i32 threshold)
+#include <string.h>
+
+#define H3 H-3
+#define W3 W*3
+#define W3_3 W*3-3
+
+void sobel_v2(u8* const cframe, u8* restrict oframe, u32 const threshold)
 {
-  i32 gx, gy, cframe_tmp, mag_approx;
-  u32 i_W3_j;
-  
   //
-  for (u64 i = 0; i < (H - 3); i++)
+  for (u32 i = 0; i < H3; i++)
   {
-    for (u64 j = 0; j < ((W * 3) - 3); j++)
+    for (u32 j = 0; j < W3_3; j++)
     {
-      i_W3_j = i * W * 3 + j;
+      u32 gx, i_W3_j, mag_approx; //gy is stored in mag_approx
+      i_W3_j = i * W3 + j;
 
-      gx = -cframe[i_W3_j];
-      gy = gx;
+      gx = ( *(cframe + i_W3_j + 8) - *(cframe + i_W3_j) );
+      mag_approx = gx;
+      gx +=           *(cframe + i_W3_j + 2) - *(cframe + i_W3_j + 6) + 2 * ( *(cframe + i_W3_j + 5) - *(cframe + i_W3_j + 3) );
+      mag_approx += - *(cframe + i_W3_j + 2) + *(cframe + i_W3_j + 6) + 2 * ( *(cframe + i_W3_j + 7) - *(cframe + i_W3_j + 1) );
 
-      cframe_tmp = cframe[i_W3_j + 2];
-      gx += cframe_tmp;
-      gy -= cframe_tmp;
-      
-      cframe_tmp = cframe[i_W3_j + 6];
-      gx -= cframe_tmp;
-      gy += cframe_tmp;
-      
-      cframe_tmp = cframe[i_W3_j + 8];
-      gx += cframe_tmp;
-      gy += cframe_tmp;
-      
-      cframe_tmp = (cframe[i_W3_j + 5] - cframe[i_W3_j + 3]);
-      cframe_tmp *= 2;
-      gx += cframe_tmp;
-      
-      cframe_tmp = (cframe[i_W3_j + 7] - cframe[i_W3_j + 1]);
-      cframe_tmp *= 2;
-      gy += cframe_tmp;
-
-      mag_approx = abs(gx)+abs(gy);
-      oframe[i_W3_j] = (mag_approx > threshold) ? 255 : (mag_approx);
+      mag_approx = abs(mag_approx) + abs(gx);
+      *(oframe + i_W3_j) = (mag_approx > threshold) ? 255 : (mag_approx);
     }
   }
 }
